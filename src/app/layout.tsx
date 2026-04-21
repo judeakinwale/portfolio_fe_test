a14c1dfe30d7925b1762b0907bad5ad142b1d361
judeakinwale <judeakinwale@gmail.com>
2026-04-21 12:45:48 +0100

    add: vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import TopNav from "@/components/layout/TopNav";
 import Footer from "@/components/layout/Footer";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const archivoBlack = Archivo_Black({
   weight: "400",
@@ -44,6 +45,7 @@ export default function RootLayout({
           <Footer />
         </div>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
